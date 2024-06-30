29a4a4179d7853b6598a59ef0ca62bfbe3bbdab9
Jay Mehta <78312356+BitsJayMehta173@users.noreply.github.com>
2024-06-30 13:41:21 +0530

    added binary format storation which decreases the size upto 85%

--- pdfshort.py
+++ pdfshort.py
@@ -1,5 +1,6 @@
 import pdfplumber
 import csv
+import os
 
 words=[]
 idx=[]
@@ -36,17 +37,38 @@ def extract_text_from_pdf(pdf_path):
 
         with open(filename, 'w', newline='') as file:
             writer = csv.writer(file)
+            cnt=0
             for key, value in dictionary.items():
                 arr=[]
                 temp=key
                 # print(temp)
                 arr.append(temp)
+                byte_arr=[]
+                byte=0
                 for v in value:
-                    arr.append(v)
+                    binary_string = format(v, '08b')
+                    bdigit=[]
+                    for w in binary_string:
+                        if w=='1':
+                            bdigit.append(True)
+                        else:
+                            bdigit.append(False)
+                    for q in range(8):
+                        if bdigit[q]:
+                            byte |= (1 << q)
+                    byte_arr.append(byte)
+                    byte=0
+                    # arr.append(binary_string)
                     # print(key)
                     # print(v)
                 # for v in value:    
                 #     print(v)
+                folder_name = 'binaryfolder'
+                file_name = str(cnt)+'.bin'
+                file_path = os.path.join(folder_name, file_name)
+                cnt+=1
+                with open(file_path, 'wb') as out_file:
+                    out_file.write(bytes(byte_arr))
                 words.append(arr)
             writer.writerows(words)
 
